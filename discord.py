236d5ec71680731bb69811e0bf459101422ddae0
Jeremy Sun <herecomesthesun1223@gmail.com>
2024-01-19 23:03:09 -0500

    Rate limit list and delete functions

--- discord.py
+++ discord.py
@@ -5,14 +5,17 @@ https://gist.github.com/adamsbytes/8445e2f9a97ae98052297a4415b5356f
 
 import json
 import aiohttp
+import time
+
+DEBUG = True
 
 class DiscordEvents:
     '''Class to create and list Discord events utilizing their API'''
-    def __init__(self, discord_token: str) -> None:
+    def __init__(self, discord_token: str, bot_id: str) -> None:
         self.base_api_url = 'https://discord.com/api/v8'
         self.auth_headers = {
             'Authorization':f'Bot {discord_token}',
-            'User-Agent': f'DiscordBot (https://discord.com/developers/applications/1198073945518260354/bot) Python/3.9 aiohttp/3.8.1',
+            'User-Agent': f'DiscordBot (https://discord.com/developers/applications/{bot_id}/bot) Python/3.9 aiohttp/3.8.1',
             'Content-Type':'application/json'
         }
 
@@ -20,20 +23,70 @@ class DiscordEvents:
         '''Returns a list of upcoming events for the supplied guild ID
         Format of return is a list of one dictionary per event containing information.'''
         event_retrieve_url = f'{self.base_api_url}/guilds/{guild_id}/scheduled-events'
+        response_list = []
         async with aiohttp.ClientSession(headers=self.auth_headers) as session:
-            try:
-                async with session.get(event_retrieve_url) as response:
-                    response.raise_for_status()
-                    assert response.status == 200
-                    response_list = json.loads(await response.read())
-            except Exception as e:
-                print(f'EXCEPTION: {e}')
-            finally:
-                await session.close()
+            status = 429
+            while status == 429:
+                try:
+                    async with session.get(event_retrieve_url) as response:
+                        if response.status == 429:
+                            if DEBUG:
+                                print('DETECTED RATE LIMIT in LIST function')
+                            reset_time = float(response.headers['X-RateLimit-Reset-After']) + 0.25
+                            time.sleep(reset_time)    
+                            continue
+                        elif response.status == 200:
+                            response_list = json.loads(await response.read())
+                            break
+                        else:
+                            session.raise_for_status()
+                except aiohttp.ClientResponseError as e:
+                    if (e.status != 429):
+                        await session.close()
+                        raise e
+            await session.close()
         return response_list
     
     async def delete_guild_events(self, guild_id: str) -> None:
-        '''Deletes all guild events labelled with GAME DAY. TO DO'''
+        '''Deletes all guild events labelled with GAME DAY.'''
+        try:
+            events = await self.list_guild_events(guild_id)
+            async with aiohttp.ClientSession(headers=self.auth_headers) as session:
+                for e in events:
+                    if 'GAME DAY' in e['name']:
+                        event_delete_url = f'{self.base_api_url}/guilds/{guild_id}/scheduled-events/{e["id"]}'
+                        status = 429
+                        while status == 429:
+                            async with session.delete(event_delete_url) as response:
+                                if response.status == 429:
+                                    if DEBUG:
+                                        print('DETECTED RATE LIMIT in DELETE function')
+                                    reset_time = float(response.headers['X-RateLimit-Reset-After']) + 0.25
+                                    time.sleep(reset_time)    
+                                    continue
+                                elif response.status == 204:
+                                    break
+                                else:
+                                    await session.raise_for_status()
+        except aiohttp.ClientResponseError as e:
+            if (e.status != 429):
+                await session.close()
+                raise e
+        finally:
+            await session.close()
+
+    async def send_message(
+        self,
+        guild_id: str,
+        channel_id: str,
+        event_name: str,
+        event_description: str,
+        event_start_time: str,
+        event_location: str
+        ) -> None:
+        '''
+        Sends message with event details to a text channel
+        '''
 
     async def create_guild_event(
         self,
@@ -60,13 +113,13 @@ class DiscordEvents:
             'entity_metadata': event_metadata,
             'entity_type': 3
         })
-        print(event_data)
 
         async with aiohttp.ClientSession(headers=self.auth_headers) as session:
             try:
                 async with session.post(event_create_url, data=event_data) as response:
                     response.raise_for_status()
                     assert response.status == 200
+                    print(f'Event \'{event_name}\' successfully created')
             except Exception as e:
                 print(f'EXCEPTION: {e}')
             finally:
--- main.py
+++ main.py
@@ -3,24 +3,30 @@ from discord import DiscordEvents
 from dotenv import load_dotenv
 import os
 
-DEBUG = True
+DEBUG = False
 
 async def main():
     load_dotenv()
     discord_token = os.environ.get('DISCORD_TOKEN')
     guild_id = os.environ.get('GUILD_ID')
     channel_id = os.environ.get('CHANNEL_ID')
+    bot_id = os.environ.get('BOT_ID')
 
     if DEBUG:
         print(discord_token)
         print(guild_id)
         print(channel_id)
+        print(bot_id)
 
-    bot = DiscordEvents(discord_token)
+    bot = DiscordEvents(discord_token, bot_id)
     # event_data = SCRAPE_WEBPAGE FUNCTION
+    # Convert datetime from local timezone to ISO standard time
+
+    await bot.delete_guild_events(guild_id)
+
     await bot.create_guild_event(
         guild_id=guild_id,
-        event_name='GAME DAY',
+        event_name='GAME DAY N',
         event_description='test description',
         event_start_time='2024-01-20T15:30:00',
         event_end_time='2024-01-20T16:30:00',
